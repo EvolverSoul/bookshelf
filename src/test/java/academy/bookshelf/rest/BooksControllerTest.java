8e6d63904f2190ea28cce50675a2a6d6dffb75a0
Jameson <composition.bell.j@gmail.com>
2019-01-23 11:50:47 +0000

    Added in another example test, switched out imports for MockMvcRequestBuilder.content(). Second test passes now

--- src/test/java/academy/bookshelf/rest/BooksControllerTest.java
+++ src/test/java/academy/bookshelf/rest/BooksControllerTest.java
@@ -2,35 +2,35 @@ package academy.bookshelf.rest;
 
 import academy.bookshelf.domain.Book;
 import academy.bookshelf.domain.Bookshelf;
-import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.assertj.core.api.Assertions.*;
-import static org.junit.Assert.*;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
 import static org.mockito.BDDMockito.given;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @RunWith(SpringRunner.class)
 //@WebMvcTest(BooksController.class)
+@AutoConfigureMockMvc
 public class BooksControllerTest {
 
-//    @Autowired
-//    private MockMvc mvc;
+    @Autowired
+    private MockMvc mvc;
 
     @MockBean
     private Bookshelf bookshelf;
@@ -40,16 +40,22 @@ public class BooksControllerTest {
         assertThat(bookshelf).isNotNull();
     }
 
-//    @Test
-//    public void testExample() throws Exception {
-//        List<Book> expectedBooks = new ArrayList<>();
-//        expectedBooks.add(new Book("book4", "someguy", "fiction", "fantasy"));
-//
-//        given(bookshelf.findAllBooks())
-//                .willReturn(expectedBooks);
-//        this.mvc.perform(get("/books").accept(MediaType.APPLICATION_JSON))
-//                .andExpect(status().isOk());
-//        // write a second expect() later to find the content of the book
-//    }
+    @Test
+    public void testExample() throws Exception {
+        List<Book> expectedBooks = new ArrayList<>();
+        expectedBooks.add(new Book("book4", "someguy", "fiction", "fantasy"));
+
+        given(bookshelf.findAllBooks())
+                .willReturn(expectedBooks);
+        this.mvc.perform(get("/books").accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
+        // write a second expect() later to find the content of the book
+    }
+
+    @Test
+    public void websiteExampleTest() throws Exception {
+        this.mvc.perform(get("/books")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().string(containsString("person1")));
+    }
 
 }
